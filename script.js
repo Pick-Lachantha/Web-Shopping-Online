6f228dc30d240683c803a763d1ea8207ed1f24b3
Bishop <mhabig1045@gmail.com>
2023-06-18 04:16:15 +0700

    Update

---
+++ script.js
@@ -0,0 +1,15 @@
+var product = [{
+    id: 1,
+    img: 'https://cdn.pixabay.com/photo/2020/10/22/10/42/hemp-5675705_640.jpg',
+    name: 'กัญชา',
+    price: 500,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium non aliquid cum iste, debitis odit.',
+    type:'ต้นกัญชา'
+},{
+    id: 2,
+    img: 'https://cdn.pixabay.com/photo/2017/03/25/19/57/marijuana-2174302_1280.jpg',
+    name: 'กัญชา',
+    price: 800,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium non aliquid cum iste, debitis odit.',
+    type:'กัญชาอัดแท่ง'
+},{}]
