76f37e25870fbcd37ebc7bd253a23724ba1e7c88
Bishop <mhabig1045@gmail.com>
2023-06-22 22:20:42 +0700

    Update db.php

---
+++ api/db.php
@@ -0,0 +1,17 @@
+<?php 
+    $db_host = 'localhost';
+    $db_name = 'shoping';
+    $db_user = 'root';
+    $db_pass = '';
+
+    header('Content-Type: application/json');
+
+    try{
+        $db = new PDO("mysql:host=${db_host}; dbname=${db_name}", $db_user, $db_pass);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "database is conected";
+    }
+    catch(PEOException $e){
+        echo $e->getMessage();
+    }
+?>
