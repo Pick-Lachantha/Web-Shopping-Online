d03f14c64eede15ff2220105f5ee45ce3696386d
Bishop <mhabig1045@gmail.com>
2023-06-22 23:08:01 +0700

    Update getallproduct.php

--- api/getallproduct.php
+++ api/getallproduct.php
@@ -2,10 +2,10 @@
     require_once('./db.php');
    
     try{
-      if($_SERVER['REQUEST_METHOD']=='GET') {
+      if($_SERVER['REQUEST_METHOD'] == 'GET') {
         $object = new stdClass;
 
-        $stmt =$db->prepare('selec * from sp_product order by id desc');
+        $stmt = $db->prepare('select * from sp_product order by id desc');
 
         if($stmt->execute()){
             $num = $stmt->rowCount();
@@ -14,31 +14,43 @@
                 $object->Result = array();
                 while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
                     extract($row);
-                    $items = array(
+                    /*$items = array(
                         "thisisname" => $name,
-                        "thisisname" => $price,
+                        "thisisprice" => $price,
                     );
                     array_push($object->Result, $items);
+                    */
+                    array_push($object->Result, $row);
+                    
 
                 }
                 $object->RespCode = 200;
                 $object->RespCode = 'success';
-                http_response_code(200)
+                http_response_code(200);
             }
             else{
-
+                $object->RespCode = 400;
+                $object->Log = 0;
+                $object->RespCode = 'bad : Not Fount data';
+                http_response_code(400);
             }
+
+            echo json_encode($object);
 
         }
         else{
-
+                $object->RespCode = 500;
+                $object->Log = 1;
+                $object->RespCode = 'bad : bad sql';
+                http_response_code(400);
         }
       }
       else{
-
+        http_response_code(405);
       }
     }
     catch(PEOException $e){
+        http_response_code(500);
         echo $e->getMessage();
     }
 ?>
