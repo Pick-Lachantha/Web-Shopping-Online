366e1f073bbab238b7356959a3449476649547a9
Bishop <mhabig1045@gmail.com>
2023-06-25 00:16:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,57 @@
- /*var product = [{
-     id: 1,
-     img: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
-     name: 'Nike',
-     price: 7000,
-     description: 'Nike Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
-     type: 'shoe'
-
- }, {
-
-     id: 2,
-     img: 'https://images.unsplash.com/photo-1511746315387-c4a76990fdce?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
-     name: 'Adidas shirt',
-     price: 1500,
-     description: 'Adidas shirt Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
-     type: 'shirt'
- }, {
-     id: 3,
-     img: 'https://images.unsplash.com/photo-1593287073863-c992914cb3e3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
-     name: 'Adidas shoe',
-     price: 45000,
-     description: 'Adidas shoe Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
-     type: 'shoe'
-}];*/
+// var product = [{
+//     id: 1,
+//     img: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
+//     name: 'Nike',
+//     price: 7000,
+//     description: 'Nike Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+//     type: 'shoe'
+// }, {
+//     id: 2,
+//     img: 'https://images.unsplash.com/photo-1511746315387-c4a76990fdce?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740',
+//     name: 'Adidas shirt',
+//     price: 1500,
+//     description: 'Adidas shirt Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+//     type: 'shirt'
+// }, {
+//     id: 3,
+//     img: 'https://images.unsplash.com/photo-1593287073863-c992914cb3e3?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774',
+//     name: 'Adidas shoe',
+//     price: 45000,
+//     description: 'Adidas shoe Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+//     type: 'shoe'
+// }];
 
 var product;
 
-//แสดง Product
-$(document).ready(() =>{
+// [{},{},{}] // length = 3
+
+$(document).ready(() => {
 
     $.ajax({
         method: 'get',
         url: './api/getallproduct.php',
-        success: function(response){
+        success: function(response) {
             console.log(response)
-            if(response.ResCode == 200){
+            if(response.RespCode == 200) {
 
                 product = response.Result;
 
-                var html = ''
-                    for(let i = 0; i < product.length; i++){
-                        html += `<div onclick="openProductDetail(${i})"class="product-items ${product[i].type}">
-                        <img src="./imgs/${product[i].img}" alt="" class="product-img">
-                        <p style="font-size: 1.2vw;">${product[i].name}</p>
-                        <p style="font-size: .9vw;">${numberWithCommas(product[i].price)}</p></div>`
-                    }
-                    $("#productlist").html(html)
+                var html = '';
+                for (let i = 0; i < product.length; i++) {
+                    html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                                <img class="product-img" src="./imgs/${product[i].img}" alt="">
+                                <p style="font-size: 1.2vw;">${product[i].name}</p>
+                                <p stlye="font-size: 1vw;">${ numberWithCommas(product[i].price) } THB</p>
+                            </div>`;
+                }
+                $("#productlist").html(html);
             }
-        }, error: function(err){
+        }, error: function(err) {
             console.log(err)
         }
     })
-
 })
 
-//Commas
 function numberWithCommas(x) {
     x = x.toString();
     var pattern = /(-?\d+)(\d{3})/;
@@ -62,7 +60,6 @@ function numberWithCommas(x) {
     return x;
 }
 
-//Search
 function searchsomething(elem) {
     // console.log('#'+elem.id)
     var value = $('#'+elem.id).val()
@@ -86,47 +83,45 @@ function searchsomething(elem) {
 
 }
 
-
-//ปุ่ม เมนู
-function searchproduct(param){
+function searchproduct(param) {
     console.log(param)
-    $(".product-items").css('display','none')
-    if(param == 'all'){
-        $(".product-items").css('display','block')
+    $(".product-items").css('display', 'none')
+    if(param == 'all') {
+        $(".product-items").css('display', 'block')
     }
-    else{
-        $("."+param).css('display','block')
+    else {
+        $("."+param).css('display', 'block')
     }
 }
 
-
-//รายละเอียดสินค้า
-var productindex = 0
-function openProductDetail(index){
-    productindex = index
-    $("#modalDesc").css('display','flex')
-    $("#mdd-img").attr('src',product[index].img)
+var productindex = 0;
+function openProductDetail(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#modalDesc").css('display', 'flex')
+    $("#mdd-img").attr('src', './imgs/' + product[index].img);
     $("#mdd-name").text(product[index].name)
-    $("#mdd-price").text(numberWithCommas(product[index].price)+ ' THB')
+    $("#mdd-price").text( numberWithCommas(product[index].price) + ' THB')
     $("#mdd-desc").text(product[index].description)
 }
 
-//ปุ่มปิด
-function closeModal(){
+function closeModal() {
     $(".modal").css('display','none')
 }
 
-//ปุ่ม AddToCart หรือ เพิ่มใส่ตระกล้า
-var cart = []
-function AddToCart(){
-    var pass = true
-    for(let i = 0; i < cart.length; i++){
-        if(productindex == cart[i].index){
-            cart[i].count++
-            pass = false
+var cart = [];
+function addtocart() {
+    var pass = true;
+
+    for (let i = 0; i < cart.length; i++) {
+        if( productindex == cart[i].index ) {
+            console.log('found same product')
+            cart[i].count++;
+            pass = false;
         }
     }
-    if(pass){
+
+    if(pass) {
         var obj = {
             index: productindex,
             id: product[productindex].id,
@@ -134,87 +129,122 @@ function AddToCart(){
             price: product[productindex].price,
             img: product[productindex].img,
             count: 1
-        }
+        };
+        // console.log(obj)
         cart.push(obj)
     }
     console.log(cart)
 
     Swal.fire({
-        icon:'success',
-        title:'Add ' + product[productindex].name + ' Ta Cart !'
+        icon: 'success',
+        title: 'Add ' + product[productindex].name + ' to cart !'
     })
-    $("#cartcout").css('display','flex').text(cart.length)
+    $("#cartcount").css('display','flex').text(cart.length)
 }
 
-//ปุ่ม cart  หรือ ตระกล้า 
-function opencart(){
+function openCart() {
     $('#modalCart').css('display','flex')
-    rendercart()
+    rendercart();
 }
 
-//rendercart
-function rendercart(){
-    if(cart.length > 0){
-        var html = ''
-        for(let i = 0; i < cart.length; i++){
+function rendercart() {
+    if(cart.length > 0) {
+        var html = '';
+        for (let i = 0; i < cart.length; i++) {
             html += `<div class="cartlist-items">
                         <div class="cartlist-left">
-                            <img src="${cart[i].img}" alt="" >
-                            <div class="cartlisrt-detail">
+                            <img src="./imgs/${cart[i].img}" alt="">
+                            <div class="cartlist-detail">
                                 <p style="font-size: 1.5vw;">${cart[i].name}</p>
-                                <p style="font-size: 1.2vw;">${numberWithCommas(cart[i].price * cart[i].count)+ ' THB'}</p>
+                                <p style="font-size: 1.2vw;">${ numberWithCommas(cart[i].price * cart[i].count) } THB</p>
                             </div>
                         </div>
-                        
                         <div class="cartlist-right">
                             <p onclick="deinitems('-', ${i})" class="btnc">-</p>
-                            <p id="countitems${i}">${cart[i].count}</p>
+                            <p id="countitems${i}" style="margin: 0 20px;">${cart[i].count}</p>
                             <p onclick="deinitems('+', ${i})" class="btnc">+</p>
                         </div>
-                    </div> `
+                    </div>`;
         }
-        $("#MyCart").html(html)
+        $("#mycart").html(html)
     }
-    else{
-        $("#MyCart").html('<p>Not found Product list</p>')
+    else {
+        $("#mycart").html(`<p>Not found product list</p>`)
     }
 }
 
-//ปุ่ม - +
-function deinitems(action, index){
-    if(action == '-'){
-        if(cart[index].count > 0){
+function deinitems(action, index) {
+    if(action == '-') {
+        if(cart[index].count > 0) {
             cart[index].count--;
             $("#countitems"+index).text(cart[index].count)
 
-            if(cart[index].count <=0) {
+            if(cart[index].count <= 0) {
                 Swal.fire({
                     icon: 'warning',
-                    title: 'Are You Sure To Dalate?',
+                    title: 'Are you sure to delete?',
                     showConfirmButton: true,
                     showCancelButton: true,
-                    ConfirmButton: 'Delete',
-                    CancelButton: 'Cancel',
-                }).then((res) =>{
-                    if(res.isConfirmed){
-                        cart.splice(index, 1)
-                        rendercart()
-                        $("#cartcout").css('display','flex').text(cart.length)
-
-                        if(cart.length <= 0){
-                            $("#cartcout").css('display','none')
-                        }
-                    }
-                    else{
-                        cart[index].count++
-                        $("#countitems"+index).text(cart[index].count)
-                    }
+                    confirmButtonText: 'Delete',
+                    cancelButtonText: 'Cancel'
+                }).then((res) => {
+                  if(res.isConfirmed) {
+                     cart.splice(index, 1) 
+                     console.log(cart)
+                     rendercart();
+                     $("#cartcount").css('display','flex').text(cart.length)
+                     
+                     if(cart.length <= 0) {
+                        $("#cartcount").css('display','none')
+                     }
+                  }  
+                  else {
+                    cart[index].count++;
+                    $("#countitems"+index).text(cart[index].count)
+                  }
                 })
             }
         }
     }
-    else if(action == '+'){
-        cart[index].count++
+    else if(action == '+') {
+        cart[index].count++;
         $("#countitems"+index).text(cart[index].count)
     }
+}
+
+function buynow() {
+    $.ajax({
+        method: 'post',
+        url: './api/buynow.php',
+        data: {
+            product: cart
+        }, success: function(response) {
+            console.log(response)
+            if(response.RespCode == 200) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Thank you',
+                    html: ` <p> Amount : ${response.Amount.Amount}</p>
+                            <p> Shipping : ${response.Amount.Shipping}</p>
+                            <p> Vat : ${response.Amount.Vat}</p>
+                            <p> Netamount : ${response.Amount.Netamount}</p>
+                            `
+                }).then((res) => {
+                    if(res.isConfirmed) {
+                        cart = [];
+                        closeModal();
+                        $("#cartcount").css('display','none')
+                    }
+                })
+            }
+            else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Something is went wrong!'
+                })
+            }
+        }, error: function(err) {
+            console.log(err)
+        }
+    })
 }
