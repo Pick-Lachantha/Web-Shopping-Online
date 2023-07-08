49c5de4786f573215a5eea0e6386a7dd0f5c698b
Bishop <mhabig1045@gmail.com>
2023-07-08 21:36:55 +0700

    Update

--- api/db.php
+++ api/db.php
@@ -1,18 +1,18 @@
-<?php
+<?php 
     $db_host = 'localhost';
     $db_name = 'shoping';
     $db_user = 'root';
     $db_pass = '';
 
     header('Content-Type: application/json');
-    date_default_timezone_set("Asia/Bangkok");
 
-    try {
-        $db = new PDO("mysql:host=${db_host}; dbname=${db_name}", $db_user, $db_pass);
+    try{
+        $db = new PDO("mysql:host=${db_host}; dbname=${dbname}", $db_user, $db_pass);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // echo "database is connected";
+
+        echo"database is cinnected";
     }
-    catch(PEOException $e) {
+    catch(PDOException  $e){
         echo $e->getMessage();
     }
 ?>
