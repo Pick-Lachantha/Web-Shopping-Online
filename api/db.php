ad954c10a038a78a4d1e5acfa5bb11b3460e4e4b
Bishop <mhabig1045@gmail.com>
2023-07-09 21:12:51 +0700

    Update

--- api/db.php
+++ api/db.php
@@ -10,7 +10,7 @@
         $db = new PDO("mysql:host=${db_host}; dbname=${dbname}", $db_user, $db_pass);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        echo"database is cinnected";
+        //echo"database is cinnected";
     }
     catch(PEOException  $e){
         echo $e->getMessage();
