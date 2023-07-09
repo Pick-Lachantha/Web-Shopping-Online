12b405d34914c80a1a4925bf3f2166322f4e17aa
Bishop <mhabig1045@gmail.com>
2023-07-09 21:16:02 +0700

    Update getallproduct.php

--- api/getallproduct.php
+++ api/getallproduct.php
@@ -5,9 +5,19 @@
         if($_SERVER['REQUEST_METHOD']=='GET'){
             $object = new stdClass();
 
-            $stmt = $db->prepare('');
-        }
-        else{
+            $stmt = $db->prepare('selec * form sp_product order by id desc');
+
+            if($stmt->execute()){
+                $num = $stmt->rowCount();
+                if ($num > 0) {
+                    
+                }else{
+
+                }
+            }else{
+
+            }
+        }else{
 
         }
     }
